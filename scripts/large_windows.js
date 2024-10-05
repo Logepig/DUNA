a56f5e8f4bb1f593522a875401972d90a13edbb4
Lenovo <ilia_vinok@mail.ru>
2024-10-05 23:19:31 +0300

    idk what is it

--- scripts/large_windows.js
+++ scripts/large_windows.js
@@ -1,30 +1,3 @@
-//6
-document.getElementById("open-add").addEventListener("click", function() {
-    document.getElementById("add-modal-block").classList.add("open")
-})
-
-//5
-
-
-document.getElementById("close-add").addEventListener("click", function() {
-    document.getElementById("add-modal-block").classList.remove("open")
-})
-
-//4
-
-document.getElementById("commit-add").addEventListener("click", function() {
-    document.getElementById("add-modal-block").classList.remove("open")
-})
-
-//3
-
-window.addEventListener('keydown', (e) => {
-    if(e.key === "Escape") {
-        document.getElementById("add-modal-block").classList.remove("open")
-    }
-})
-
-
 document.querySelectorAll(".button_status").forEach(el => {
     el.addEventListener("click", function() {
         document.getElementById("add-modal-block_of_blogers").classList.add("open")
@@ -51,13 +24,3 @@ document.querySelectorAll(".button_status1").forEach(el => {
     })
 }
 )
-
-
-
-
-
-
-
-
-
-
