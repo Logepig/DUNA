2eb596874ff17dd99b1e88885ead373c7de6b26a
Lenovo <ilia_vinok@mail.ru>
2024-12-14 17:11:18 +0300

    windows_shit

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,29 +1,3 @@
-//6
-document.getElementById("open-add").addEventListener("click", function() {
-    document.getElementById("add-modal-block").classList.add("open")
-})
-
-//5
-
-
-document.getElementById("close-add").addEventListener("click", function() {
-    document.getElementById("add-modal-block").classList.remove("open")
-})
-
-//4
-
-document.getElementById("commit-add").addEventListener("click", function() {
-    document.getElementById("add-modal-block").classList.remove("open")
-})
-
-//3
-
-window.addEventListener('keydown', (e) => {
-    if(e.key === "Escape") {
-        document.getElementById("add-modal-block").classList.remove("open")
-    }
-})
-
 const butt2 = document.querySelectorAll('.button_status');
 
 butt2.forEach(butt => {
@@ -59,7 +33,3 @@ buttons.forEach(button => {
         }
     });
 });
-
-
-
-
